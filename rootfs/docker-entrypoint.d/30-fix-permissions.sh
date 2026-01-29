25d9399c8d46b6f84cf5d14826a2fbb361fa787e
Emily Flion <emneo@kreog.com>
2026-01-29 16:13:29 +0100

    No fixing those permissions please
    Signed-off-by: Emily Flion <emneo@kreog.com>

--- rootfs/docker-entrypoint.d/30-fix-permissions.sh
+++ rootfs/docker-entrypoint.d/30-fix-permissions.sh
@@ -2,15 +2,13 @@
 # Script to fix file permissions. Loaded during Docker entrypoint.
 set -eu
 
-chown -R ${CGIT_APP_USER}:${CGIT_APP_USER} /opt/git/ \
-                     /opt/cgit/ \
+chown -R ${CGIT_APP_USER}:${CGIT_APP_USER} /opt/cgit/ \
                      /run/fcgiwrap/
 
 chmod 770 /opt/cgit/ \
           /opt/cgit/ \
           /opt/cgit/filters/ \
           /opt/cgit/app/ \
-          /opt/cgit/cache/ \
-          /opt/git/
+          /opt/cgit/cache/
 
 chmod u+x /opt/cgit/app/cgit.cgi
